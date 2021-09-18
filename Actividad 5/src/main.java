c8469f668386e3642b8312a3f7b7015cd0aca545
JaviCamacho <90795084+JaviCamacho@users.noreply.github.com>
2021-09-17 19:37:59 -0500

    Actividad 5

---
+++ Actividad 5/src/main.java
@@ -0,0 +1,13 @@
+public class main {
+    public static void main(String[] args) {
+        int x=50;
+        int y=37;
+        System.out.println("X= "+x);
+        System.out.println("Y= "+y);
+        System.out.println("Resultado de suma= "+(x+y));
+        System.out.println("Resultado de resta= "+(x-y));
+        System.out.println("Resultado de multiplicación= "+(x*y));
+        System.out.println("Resultado de división= "+(x/y));
+        System.out.println("Resultado de módulo= "+(x%y));
+    }
+}
